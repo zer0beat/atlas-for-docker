6617790b6e019c5f03b30f5e6a822945a9bfdbd0
Joel Llacer Martínez <z0beat@gmail.com>
2020-05-23 17:32:04 +0200

    Update stack

--- samples/atlas/scripts/00_create_atlas_collections.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-solr create -p 8983 -c vertex_index -shards 1 -replicationFactor 1 
-solr create -p 8983 -c edge_index -shards 1 -replicationFactor 1 
-solr create -p 8983 -c fulltext_index -shards 1 -replicationFactor 1 
---
+++ samples/atlas/scripts/00_create_solr_collections.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl 'http://solr:8983/solr/admin/collections?action=CREATE&name=vertex_index&numShards=1&collection.configName=_default'
+curl 'http://solr:8983/solr/admin/collections?action=CREATE&name=edge_index&numShards=1&collection.configName=_default'
+curl 'http://solr:8983/solr/admin/collections?action=CREATE&name=fulltext_index&numShards=1&collection.configName=_default'
