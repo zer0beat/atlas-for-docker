3e268c26b2f462f5522b3a30bbfbdd6b1d817b51
Joel Llacer Martínez <z0beat@gmail.com>
2020-06-01 17:05:36 +0200

    Add dev image

--- test/tests/atlas-run/run.sh
+++ test/tests/atlas-run/run.sh
@@ -15,4 +15,7 @@ project_directory="${dir}/../../../samples/atlas-kafka-cassandra-solr"
 
 ATLAS_IMAGE=$image docker-compose --project-directory $project_directory -f $project_directory/docker-compose.yml up -d
 
+# Wait until atlas is available (with timeout)
+# Call to the API
+
 docker-compose --project-directory $project_directory -f $project_directory/docker-compose.yml down
