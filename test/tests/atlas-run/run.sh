7316f2e3c5a99d81ac2113af1939bd6c2b07f82a
Joel Llacer Martínez <z0beat@gmail.com>
2020-06-02 00:01:38 +0200

    Update many things, it's not a great commit

--- test/tests/atlas-run/run.sh
+++ test/tests/atlas-run/run.sh
@@ -7,15 +7,17 @@ fi
 
 dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-self="$(basename "$0")"
-
 image=$1
 
-project_directory="${dir}/../../../samples/atlas-kafka-cassandra-solr"
-
-ATLAS_IMAGE=$image docker-compose --project-directory $project_directory -f $project_directory/docker-compose.yml up -d
+if [[ "$image" == *-dev ]]; then
+    cname="atlas-container-$RANDOM-$RANDOM"
+    docker run -d --name "$cname" -p "21000:21000" "$image"
+    trap "docker rm -vf $cname > /dev/null" EXIT
+else
+    project_directory="${dir}/../../../samples/atlas-kafka-cassandra-solr"
+    ATLAS_IMAGE=$image docker-compose --project-directory $project_directory -f $project_directory/docker-compose.yml up -d
+    trap "docker-compose --project-directory $project_directory -f $project_directory/docker-compose.yml down > /dev/null" EXIT
+fi
 
 # Wait until atlas is available (with timeout)
 # Call to the API
-
-docker-compose --project-directory $project_directory -f $project_directory/docker-compose.yml down
